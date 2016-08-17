7eae26c8c31c781d846b1a987536e94513ad8082
Stacy Andrews <stacy.andrews@iinet.net.au>
2016-08-17 11:52:33 +0800

    minor cleanup

--- src/main.rs
+++ src/main.rs
@@ -13,47 +13,41 @@ use std::io::Read;
 
 fn main() {
     loop {
-        let client = Client::new();
+        let playlist = get_playlist();
 
-        let mut res = client.get("http://music.abcradio.net.au/api/v1/plays/doublej/now.json").send().unwrap();
-        let mut buffer = String::new();
-        res.read_to_string(&mut buffer).unwrap();
-
-        let data: Value = serde_json::from_str(&buffer).unwrap();
-
-        let obj = data.as_object().unwrap();
-        let now = obj.get("now").unwrap().as_object().unwrap();
-
-        let recording = now.get("recording").unwrap().as_object().unwrap();
-
-        let artists = recording.get("artists").unwrap().as_array().unwrap();
-        let artists_iter: Vec<_> = artists.into_iter()
-        .map(|x| x.as_object().unwrap().get("name").unwrap().as_string().unwrap())
-        .collect();
-
-        println!("name {:?}", artists_iter.concat());
-
-        let title = recording.get("title").unwrap();
-
-        println!("title {:?}", title);
+        print_song(playlist);
 
         let five_seconds = Duration::new(5, 0);
         thread::sleep(five_seconds);
     }
 }
 
-// {
-//     "FirstName": "John",
-//     "LastName": "Doe",
-//     "Age": 43,
-//     "Address": {
-//         "Street": "Downing Street 10",
-//         "City": "London",
-//         "Country": "Great Britain"
-//     },
-//     "PhoneNumbers": [
-//         "+44 1234567",
-//         "+44 2345678"
-//     ]
-// }
+fn get_playlist() -> String {
+    let client = Client::new();
 
+    let mut res = client.get("http://music.abcradio.net.au/api/v1/plays/doublej/now.json").send().unwrap();
+    let mut buffer = String::new();
+    res.read_to_string(&mut buffer).unwrap();
+
+    return buffer;
+}
+
+fn print_song(playlist: String) {
+    let data: Value = serde_json::from_str(&playlist).unwrap();
+
+    let obj = data.as_object().unwrap();
+    let now = obj.get("now").unwrap().as_object().unwrap();
+
+    let recording = now.get("recording").unwrap().as_object().unwrap();
+
+    let artists = recording.get("artists").unwrap().as_array().unwrap();
+    let artists_iter: Vec<_> = artists.into_iter()
+        .map(|x| x.as_object().unwrap().get("name").unwrap().as_string().unwrap())
+        .collect();
+
+    println!("name {:?}", artists_iter.concat());
+
+    let title = recording.get("title").unwrap();
+
+    println!("title {:?}", title);
+}
